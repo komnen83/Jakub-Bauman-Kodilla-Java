433c1599c0dbf061d2f42817495d15c71f42193a
Kuba <kuba.bauman@gmail.com>
2018-06-13 23:13:21 +0200

    Calculator

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,61 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testAdd() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.add(4, 5);
+        //Then
+        Assert.assertEquals(9, result, 0.0);
+    }
+
+    @Test
+    public void testSub() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.sub(4, 5);
+        //Then
+        Assert.assertEquals(-1, result, 0.0);
+    }
+
+    @Test
+    public void testMul() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.mul(4, 5);
+        //Then
+        Assert.assertEquals(20, result, 0.0);
+    }
+
+    @Test
+    public void testDiv() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.div(4, 5);
+        //Then
+        Assert.assertEquals(0.8, result, 0.0);
+    }
+}
