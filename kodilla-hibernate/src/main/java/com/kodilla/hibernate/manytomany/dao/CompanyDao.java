c96d61d99c076ccbc7161c6de3eecf1b2a39e507
Kuba <kuba.bauman@gmail.com>
2018-07-11 21:57:09 +0200

    CompanyDao

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/CompanyDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/CompanyDao.java
@@ -1,7 +1,6 @@
 package com.kodilla.hibernate.manytomany.dao;
 
 import com.kodilla.hibernate.manytomany.Company;
-import com.kodilla.hibernate.manytomany.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -15,8 +14,5 @@ public interface CompanyDao extends CrudRepository<Company, Integer> {
 
     @Query(nativeQuery = true)
     List<Company> showFirstThreeLetters();
-
-//    @Query
-//    List<Employee> seekLastname();
 
 }
