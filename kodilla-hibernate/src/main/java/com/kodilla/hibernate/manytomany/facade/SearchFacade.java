b2b0ccc52de168f132beb7d8b7cb4e05bad16dd7
Kuba <kuba.bauman@gmail.com>
2018-08-20 20:03:31 +0200

    Search facade

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/SearchFacade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/SearchFacade.java
@@ -7,6 +7,8 @@ import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SearchFacade {
 
@@ -14,16 +16,20 @@ public class SearchFacade {
     private Employee employee;
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SearchFacade.class);
 
-    public void searchProcess(final CompanyDao companyDao, final EmployeeDao employeeDao) throws SearchException{
+    public List<Company> searchCompany(final CompanyDao companyDao) throws SearchException {
         boolean wasError = false;
         LOGGER.info("Searching company name" + company.getName());
-        if(!companyDao.findMatchingName(String "NAME")) {
+        if (!companyDao.findMatchingName(String "Nie wiem co tutaj za argumentu uzyc")) {
             LOGGER.error(SearchException.ERR_COMPANY_NOT_FOUND);
             wasError = false;
             throw new SearchException(SearchException.ERR_COMPANY_NOT_FOUND);
         }
+    }
+
+    public List<Employee> searchEmployee(final EmployeeDao employeeDao) throws  SearchException {
+        boolean wasError = false;
         LOGGER.info("Searching employee lastname" + employee.getLastname());
-        if(!employeeDao.findMathingName(String "NAME")) {
+        if(!employeeDao.findMatchingName(String "Nie wiem co tutaj za argumentu uzyc")) {
             LOGGER.error(SearchException.ERR_EMPLOYEE_NOT_FOUND);
             wasError = false;
             throw new SearchException(SearchException.ERR_EMPLOYEE_NOT_FOUND);
